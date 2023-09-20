6a56a504029f71261b66ea9cacb8727b9a9dffb1
andrew-diagilev <it@algos.com.ua>
2023-09-20 13:18:28 +0300

    Authorization imp 09.20.2023

--- components/Navigator.js
+++ components/Navigator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext} from "react";
 import Main from '../views/Main';
 import Profile from '../views/Profile';
 import Abstracts from '../views/Abstracts';
@@ -10,33 +10,49 @@ import Test from '../views/Test';
 import NewView2 from '../views/NewView2';
 import LessonsN from '../views/2';
 import One from '../views/1';
-import {NavigationContainer} from '@react-navigation/native';
+import Auth from '../views/Auth';
+import {useIsFocused} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as RootNavigator from "./RootNavigator";
+import {AuthContext} from './AuthContext';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigate() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{
-                cardStyle: { backgroundColor: 'transparent', headerTransparent: true, }, // Установите прозрачный фон
+    const {isLoading, token} = useContext(AuthContext);
 
-                headerStyle: { backgroundColor: 'transparent' }, // Установите прозрачный фон для заголовков
-                 // Настройки цвета заголовков
-            }} >
-                <Stack.Screen name="Main" component={Main} options={{headerShown: true, headerTransparent: true}} />
-                <Stack.Screen name="Lessons" component={Lessons} options={{headerShown: true, headerTransparent: true}}/>
-                <Stack.Screen name="Tests" component={Tests}/>
-                <Stack.Screen name="Profile" component={Profile}/>
-                <Stack.Screen name="Abstracts" component={Abstracts}/>
-                <Stack.Screen name="Lesson" component={Lesson}/>
-                <Stack.Screen name="Abstract" component={Abstract}/>
-                <Stack.Screen name="Test" component={Test}/>
-                <Stack.Screen name="One" component={One}/>
-                <Stack.Screen name="LessonsN" component={LessonsN}/>
-                <Stack.Screen name="NewView2" component={NewView2} options={{headerShown: false}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+    if (isLoading) {
+        return null
+    }
+
+    return (
+
+        <Stack.Navigator screenOptions={{
+            cardStyle: {backgroundColor: 'transparent', headerTransparent: true,}, // Установите прозрачный фон
+
+            headerStyle: {backgroundColor: 'transparent'}, // Установите прозрачный фон для заголовков
+            // Настройки цвета заголовков
+        }}>
+            {token ? (<>
+                    <Stack.Screen name="Main" component={Main} options={{headerShown: true, headerTransparent: true}}/>
+                    <Stack.Screen name="Lessons" component={Lessons}
+                                  options={{headerShown: true, headerTransparent: true}}/>
+                    <Stack.Screen name="Tests" component={Tests}/>
+                    <Stack.Screen name="Profile" component={Profile}/>
+                    <Stack.Screen name="Abstracts" component={Abstracts}/>
+                    <Stack.Screen name="Lesson" component={Lesson}/>
+                    <Stack.Screen name="Abstract" component={Abstract}/>
+                    <Stack.Screen name="Test" component={Test}/>
+                    <Stack.Screen name="One" component={One}/>
+                    <Stack.Screen name="LessonsN" component={LessonsN}/>
+                    <Stack.Screen name="NewView2" component={NewView2} options={{headerShown: false}}/></>)
+                :
+                (<><Stack.Screen name="Auth" component={Auth} options={{headerShown: false}}/>
+                    <Stack.Screen name="Main" component={Main}
+                                  options={{headerShown: true, headerTransparent: true}}/></>)}
+        </Stack.Navigator>
     );
 }
 
