7061879c2f218ad8f01612f0714b95ff0adbf0ea
aleks <cancrin@gmail.com>
2023-10-11 12:34:04 +0300

    23.10.11 12:33

--- views/Profile.js
+++ views/Profile.js
@@ -6,6 +6,7 @@ import {useDispatch} from 'react-redux';
 import {logout} from "../redux/authActions";
 import HeaderLessons from "./Headers";
 import {LogoSvg, CatSvg} from "../assets/imgsvg";
+import CircularProgress from "./ProgressCircle";
 
 
 export default function Profile({navigation}) {
@@ -15,6 +16,44 @@ export default function Profile({navigation}) {
         dispatch(logout());
     };
 
+    const LessonTotal = 25;
+    const TestTotal = 25;
+    const TestQuestion = 4;
+    const BlockTotal = 5;
+    const BlockQuestion = 10;
+
+// Генерирует случайное целое число в диапазоне [0, LessonTotal]
+    const LessonPass = Math.floor(Math.random() * (LessonTotal + 1));
+
+// Генерирует случайное целое число в диапазоне [LessonTotal - 1, LessonTotal]
+    const TestPass = Math.floor(Math.random() * (LessonPass - (LessonPass - 1) + 1)) + (LessonPass - 1);
+    if (TestPass < 0) {
+        const TestPass = 0;
+    }
+
+    const TestQuestionTotal = TestTotal * TestQuestion;
+    const TestQuestionPass = TestPass * TestQuestion;
+
+// Генерирует случайное целое число в диапазоне [0, TestQuestionPass]
+    const TestQuestionСorrect = Math.floor(Math.random() * (TestQuestionPass + 1));
+
+// Генерирует случайное целое число в диапазоне [0, TestPass / BlockTotal]
+    const BlockPass = Math.floor(Math.random() * (TestPass / BlockTotal + 1));
+
+    const BlockQuestionTotal = BlockTotal * BlockQuestion;
+    const BlockQuestionPass = BlockPass * BlockQuestion;
+
+// Генерирует случайное целое число в диапазоне [0, BlockQuestionPass]
+    const BlockQuestionСorrect = Math.floor(Math.random() * (BlockQuestionPass + 1));
+
+// Ваши случайные значения
+    console.log(`LessonPass: ${LessonPass}`);
+    console.log(`TestPass: ${TestPass}`);
+    console.log(`TestQuestionСorrect: ${TestQuestionСorrect}`);
+    console.log(`BlockPass: ${BlockPass}`);
+    console.log(`BlockQuestionСorrect: ${BlockQuestionСorrect}`);
+
+
     const goBack = () => navigation.goBack();
     return (
         <ImageBackground source={ImageBg1} resizeMode="cover" style={commonStyles.ImageBg}>
@@ -23,9 +62,31 @@ export default function Profile({navigation}) {
                     <HeaderLessons Title={'ПРОФІЛЬ'} IconLeft={CatSvg} IconRight={LogoSvg}/>
                 </View>
                 <View style={commonStyles.BodyArea}>
-                    <View style={commonStyles.ContainerLesson}>
-                        <View style={commonStyles.TitleTemp}>
-                            <Text style={commonStyles.TitleTempText}>Профіль Користувача</Text>
+
+
+                    <View style={styles.ContainerProgress}>
+                        <View style={styles.MainProgress}>
+                            <View style={styles.TitleProgress}></View>
+                            <View style={styles.BoxVerticalProgress}>
+                                <View style={styles.TitleBoxHorizontalProgress}></View>
+                                <View style={styles.BoxHorizontalProgress}>
+                                    <View style={styles.StatBoxProgress}></View>
+                                    <View style={styles.StatBoxProgress}></View>
+                                </View>
+                            </View>
+
+
+                            <View style={styles.BoxHorizontalProgress}>
+                                <View style={styles.StatBoxProgress}></View>
+                                <View style={styles.StatBoxProgress}></View>
+                            </View>
+                        </View>
+                    </View>
+
+
+                    <View style={commonStyles.TitleTemp}>
+                        {/*<Text style={commonStyles.TitleTempText}>Профіль Користувача</Text>
+                            <Text style={styles.Text2}>Реєстраційні данні</Text>
                             <Text style={styles.Text2}>Ім'я</Text>
                             <Text style={styles.Text1}>Олексій</Text>
                             <Text style={styles.Text2}>Адреса електроної пошти</Text>
@@ -37,18 +98,115 @@ export default function Profile({navigation}) {
                             <Text style={styles.Text2}>Рейтинг</Text>
                             <Text style={styles.Text1}>Загальний рейтинг</Text>
 
-                            <TouchableOpacity style={commonStyles.Button} onPress={handleLogout}>
-                                <Text style={commonStyles.ButtonText}>Вийти</Text>
-                            </TouchableOpacity>
-                        </View>
+
+
+
+
+                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                <CircularProgress
+                                    radius={40}
+                                    strokeWidth={10}
+                                    progress={TestPass}
+                                    total={TestTotal}
+                                    //text="Пройдено тестів"
+                                />
+                            </View>
+                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop:120 }}>
+                                <CircularProgress
+                                    radius={40}
+                                    strokeWidth={10}
+                                    progress={TestQuestionСorrect}
+                                    total={TestQuestionPass }
+                                   // text="Вірних відповідей"
+                                />
+                            </View>
+                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop:120 }}>
+                                <CircularProgress
+                                    radius={40}
+                                    strokeWidth={10}
+                                    progress={BlockPass}
+                                    total={BlockTotal}
+                                    //text="Підсумкових тестів"
+                                />
+                            </View>
+                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop:120 }}>
+                                <CircularProgress
+                                    radius={40}
+                                    strokeWidth={10}
+                                    progress={BlockQuestionСorrect}
+                                    total={BlockQuestionPass}
+                                    //text="Вірних відповідей"
+                                />
+                            </View>
+
+
+*/}
+
+
+                        <TouchableOpacity style={commonStyles.Button} onPress={handleLogout}>
+                            <Text style={commonStyles.ButtonText}>Вийти</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
         </ImageBackground>
-    );
+    )
+        ;
 }
 
 const styles = StyleSheet.create({
+
+    ContainerProgress: {
+        flex: 1,
+        width: '100%',
+        padding: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgray',
+    },
+    MainProgress: {
+        flex: 1,
+        width: '100%',
+        padding: 5,
+        flexDirection: 'column',
+        backgroundColor: 'gray',
+    },
+    TitleProgress: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: 'blue',
+        //margin: 10,
+    },
+
+
+    BoxVerticalProgress:{},
+
+
+    BoxHorizontalProgress: {
+        flex: 2,
+        width: '100%',
+        backgroundColor: 'green',
+        marginTop: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+    },
+
+    StatBoxProgress: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: 'red',
+        margin: 5,
+    },
+
+
+    TitleBoxHorizontalProgress: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'yellow',
+    },
+
+
     Text1: {
 
         color: '#00325B',
---
+++ views/ProgressCircle.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ProgressCircle from 'react-native-progress-circle';
+
+
+
+
+
+
+
+const CircularProgress = ({ radius, progress, total, text }) => {
+    const adjustedTotal = total === 0 ? 0.0001 : total;
+    const progressPercentage = Math.floor((progress / adjustedTotal) * 100);
+
+    return (
+        <View>
+            <ProgressCircle
+                percent={progressPercentage}
+                radius={radius}
+                borderWidth={8}
+                color="#007AFF"
+                bgColor="#ccc"
+                textStyle={styles.textStyle}
+            >
+                <Text style={styles.percentageText}>{`${progressPercentage}%`}</Text>
+                <Text style={styles.infoText}>{`${progress} / ${total}`}</Text>
+
+            </ProgressCircle>
+
+
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center', // Добавьте это свойство
+        color: '#007AFF',
+    },
+    percentageText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center', // Добавьте это свойство
+    },
+    infoText: {
+        fontSize: 12,
+        textAlign: 'center', // Добавьте это свойство
+    },
+});
+
+export default CircularProgress;
